ed6a182a6cbc435d402826e3b830547bf60c19ed
Evelina Valieva <Evelina.Valieva@urfu.me>
2024-10-04 20:35:00 +0500

    5. В трехзначном числе x зачеркнули вторую цифру. Когда к оставшемуся двузначному числу приписали эту цифру справа, получилось число n . По заданному числу n (ввод с клавиатуры, 100 999 ≤ ≤ n и число десятков не равно нулю) найти число x

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число n (100<=n<=999): ");
+
+            double n = int.Parse(Console.ReadLine());
+            double a = n / 100;
+            double b = n % 10;
+            double c = (n / 10) % 10;
+            double x = 100 * a + 10 * b + c;
+
+            Console.WriteLine($"Число x: {x}");
+            Console.ReadKey();
+        }
+    }
+}
